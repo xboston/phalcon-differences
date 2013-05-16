8e6d796678fbea79b62b9a70a1f60b3d48c34de2
xboston <bost56@gmail.com>
2013-05-17 01:16:28 +0600

    update Phalcon_DiInterface

--- differences/Phalcon_DiInterface.php
+++ differences/Phalcon_DiInterface.php
@@ -5,42 +5,6 @@
  */
 
 Phalcon\DiInterface::
-	set($alias, $definition, $shared = null);
-Phalcon\Di::
-	set($name, $definition, $shared = null);
-
-
-Phalcon\DiInterface::
-	remove($alias);
-Phalcon\Di::
-	remove($name);
-
-
-Phalcon\DiInterface::
-	attempt($alias, $definition, $shared = null);
-Phalcon\Di::
-	attempt($name, $definition, $shared = null);
-
-
-Phalcon\DiInterface::
-	get($alias, $parameters = null);
-Phalcon\Di::
-	get($name, $parameters = null);
-
-
-Phalcon\DiInterface::
-	getShared($alias, $parameters = null);
-Phalcon\Di::
-	getShared($name, $parameters = null);
-
-
-Phalcon\DiInterface::
-	has($alias);
-Phalcon\Di::
-	has($name);
-
-
-Phalcon\DiInterface::
 	offsetExists($offset);
 Phalcon\Di::
 	offsetExists($alias);
